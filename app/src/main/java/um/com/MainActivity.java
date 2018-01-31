2d448e8bf0a9004652974d171e307b79b82413ee
xuwenX <1398813354@qq.com>
2018-01-31 22:39:16 +0800

    完成了三个fragment的添加，分别作为各自的第一个界面（即功能划分）！

--- app/src/main/java/um/com/MainActivity.java
+++ app/src/main/java/um/com/MainActivity.java
@@ -1,13 +1,19 @@
 package um.com;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    以下用于定义界面控制组件，适用于三个初始界面所有内容
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
     }
 }
---
+++ app/src/main/java/um/com/right_fragment.java
@@ -0,0 +1,26 @@
+package um.com;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 沐 陽 on 2018/1/30.
+ */
+
+public class right_fragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.right_layout,container,false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
